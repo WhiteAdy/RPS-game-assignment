bf94c188cfd7d5f43a77890b0516d6f654b352cd
Adi Tamimi <atamimi@estee.com>
2023-09-12 23:20:32 +0300

    ux improvements

--- src/components/atoms/Instructions/Instructions.tsx
+++ src/components/atoms/Instructions/Instructions.tsx
@@ -7,19 +7,20 @@ export default function Instructions() {
                     Choose your player name, the progress for this name will be saved locally so
                     that you can track your progress in future sessions.
                 </li>
+                <li>You can add your own weapons and rules!</li>
+                <li>You can reset the game progress at any time.</li>
                 <li>
                     Your current score can be seen on the right side of the screen, along with the
                     leaderboard.
                 </li>
                 <li>
+                    The total score for sorting is calculated with the following formula: (wins*2) -
+                    losses + draws.
+                </li>
+                <li>
                     After you click "Begin a new round!" you will have 3 seconds to choose your
                     weapon, otherwise a random one will be chosen for you.
                 </li>
-                <li>
-                    You can add your own weapons and rules! Do that by clicking "Add weapons"
-                    button.
-                </li>
-                <li>You can reset the game progress at any time by clicking the "Reset" button.</li>
             </ul>
         </div>
     );
